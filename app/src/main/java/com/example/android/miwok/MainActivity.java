205883bd48f8ac13d301b97d7456b3d24c4eb05a
Samuel Pedro <samuelcpedro@gmail.com>
2016-08-15 08:27:20 +0000

    Use OnClickListeners For All Categories Part 1

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,8 +15,10 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,17 +29,23 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
-        /**
-         * 2. Create a new object instance of the event listener (using the constructor)
-         */
-        NumbersClickListener clickListener = new NumbersClickListener();
 
         //Find the View that shows the numbers category
         TextView numbersTextView = (TextView) findViewById(R.id.numbers);
-        /**
-         * 3. Attach the listener to the view
-         */
+
         //Set a clicklistener on that View
-        numbersTextView.setOnClickListener(clickListener);
+        //This turn to
+        //numbersTextView.setOnClickListener(new NumbersClickListener());
+        //This
+        numbersTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(view.getContext(), "Open the list of numbers", Toast.LENGTH_SHORT).show();
+                //Create a new Intent to open the {@link NumbersActivity}
+                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+                //Start the new activity
+                startActivity(numbersIntent);
+            }
+        });
     }
 }
