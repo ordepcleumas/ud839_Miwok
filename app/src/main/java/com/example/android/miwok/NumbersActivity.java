3e9cea020a4fac8410bf0c8c5dfc37b1f7c500d7
Samuel Pedro <samuelcpedro@gmail.com>
2016-08-15 12:26:32 +0000

    Use the For Loop

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -28,12 +28,10 @@ public class NumbersActivity extends AppCompatActivity {
 
         LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
 
-        int index = 0;
-        while (index < words.size()) {
+        for (int index = 0; index < words.size(); index++) {
             TextView wordView = new TextView(this);//this is NumberActivity context
             wordView.setText(words.get(index));
             rootView.addView(wordView);
-            index++;
         }
     }
 }
