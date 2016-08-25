3a0371d685cb164c2c9412514dcfc0d108c3f0c0
Samuel Pedro <samuelcpedro@gmail.com>
2016-08-25 20:34:28 +0000

    Example App with Custom ArrayAdapter

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -68,7 +68,7 @@ public class WordAdapter extends ArrayAdapter<Word> {
         // Find the ImageView in the list_item.xml layout with the ID list_item_icon
         //ImageView iconView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
         // Get the image resource ID from the current Word object and
-        // set the image to iconView
+        // set the image to iconView ---
         //iconView.setImageResource(currentWord.getImageResourceId());
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
